e7862910a65a232f7d7ac515fa0166e5495e259e
Matthew C Wake <mcwake-dev@outlook.com>
2021-11-30 10:22:05 +0000

    Completed GET /api/articles/:article_id route and tests

--- routes/articlesRouter.js
+++ routes/articlesRouter.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { getArticle } = require("../controllers/articles.controller");
 
+router.route("/:article_id").get(getArticle);
 module.exports = router;
