5d271d394814ddddfd2ee6a2ca224ede0e2623f5
Matthew C Wake <mcwake-dev@outlook.com>
2021-11-30 09:09:02 +0000

    Created test for 404 error handling

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,20 @@
-const db = require('../db/connection.js');
-const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const { expect, it, describe } = require("@jest/globals");
+const request = require("supertest");
+const db = require("../db/connection");
+const app = require("../app");
+const testData = require("../db/data/test-data/index");
+const seed = require("../db/seeds/seed");
 
-beforeEach(() => seed(testData));
-afterAll(() => db.end());
+beforeAll(() => {
+  return seed(testData);
+});
+
+afterAll(() => {
+  if (db.end) db.end();
+});
+
+describe("GET /api/sir-not-appearing/in-this-app", () => {
+  it("should return a 404 error for a non-existent endpoint", () => {
+    return request(app).get("/api/sir-not-appearing/in-this-app").expect(404);
+  });
+});
