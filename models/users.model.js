17f4cd7f33e2015a6537466abce762777aa96304
Matthew C Wake <mcwake-dev@outlook.com>
2022-02-18 15:03:41 +0000

    Added password field and split name field into first name and surname

--- models/users.model.js
+++ models/users.model.js
@@ -2,7 +2,7 @@ const db = require("../db/connection");
 
 exports.selectUsers = async () => {
   const results = await db.query(
-    `SELECT username, avatar_url, name, 
+    `SELECT username, avatar_url, firstName, surname, 
     (SELECT COUNT(*) FROM articles WHERE articles.author = users.username) AS articlecount, 
     (SELECT SUM(votes) FROM articles WHERE articles.author = users.username) AS articlevotes, 
     (SELECT COUNT(*) FROM comments WHERE comments.author = users.username) AS commentcount, 
@@ -15,8 +15,23 @@ exports.selectUsers = async () => {
 
 exports.selectUser = async (username) => {
   const results = await db.query(
-    "SELECT username, avatar_url, name FROM users WHERE username = $1 ORDER BY username;",
+    "SELECT username, avatar_url, firstName, surname, password FROM users WHERE username = $1 ORDER BY username;",
     [username]
+  );
+
+  return results.rows[0];
+};
+
+exports.createUser = async (
+  username,
+  firstName,
+  surname,
+  password,
+  avatar_url
+) => {
+  const results = await db.query(
+    "INSERT INTO users (username, firstname, surname, password, avatar_url) VALUES ($1, $2, $3, $4, $5) RETURNING *;",
+    [username, firstName, surname, password, avatar_url]
   );
 
   return results.rows[0];
