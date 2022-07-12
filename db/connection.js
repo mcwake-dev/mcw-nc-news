9bfeffe0bcd488b31ab89eb12a0107eee42ee920
Matthew C Wake <mcwake-dev@outlook.com>
2022-07-12 17:48:46 +0100

    Tweaked formatting

--- db/connection.js
+++ db/connection.js
@@ -10,13 +10,14 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-const config = ENV === "production"
-  ? {
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  }
-  : {};
+const config =
+  ENV === "production"
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 module.exports = new Pool(config);
